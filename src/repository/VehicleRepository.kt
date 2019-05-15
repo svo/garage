eb3aa04f1cbae2ef1b8124635d653e35d5c684d7
Sean Van Osselaer <sean.vanosselaer@myob.com>
2019-05-15 17:35:00 +1000

    Working with 'Exposed' for relational database access

--- src/repository/VehicleRepository.kt
+++ src/repository/VehicleRepository.kt
@@ -1,10 +1,27 @@
 package `is`.qual.repository
 
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.Table
 import `is`.qual.model.Vehicle
 import `is`.qual.model.VehicleType
 
+object VehicleTable : Table("vehicle") {
+    val id = integer("id").autoIncrement().primaryKey()
+    val type = varchar("type", 16)
+}
+
 open class VehicleRepository {
+
     open fun getAll(): List<Vehicle> {
-        return listOf(Vehicle(VehicleType.CAR))
+        return VehicleTable.selectAll().map {
+            Vehicle(VehicleType.valueOf(it[VehicleTable.type]))
+        }
+    }
+
+    fun save(vehicle: Vehicle): Int {
+        return VehicleTable.insert {
+            it[type] = vehicle.type.name
+        } get VehicleTable.id
     }
 }
--- test/repository/VehicleRepositoryTest.kt
+++ test/repository/VehicleRepositoryTest.kt
@@ -1,15 +1,39 @@
 package `is`.qual.repository
 
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import `is`.qual.model.Vehicle
 import `is`.qual.model.VehicleType
+import `is`.qual.repository.VehicleTable
 
 class VehicleRepositoryTest {
     @Test
-    fun shouldHaveExpectedVehicle() {
+    fun shouldSaveVehicle() {
+        val expected = 1
+
+        Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+
+        transaction {
+            SchemaUtils.create(VehicleTable)
+            assertEquals(expected, VehicleRepository().save(Vehicle(VehicleType.CAR)))
+        }
+    }
+
+    @Test
+    fun shouldHaveExpectedVehicles() {
         val expected = listOf(Vehicle(VehicleType.CAR))
 
-        assertEquals(expected, VehicleRepository().getAll())
+        Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+
+        transaction {
+            SchemaUtils.create(VehicleTable)
+            VehicleTable.insert { it[type] = VehicleType.CAR.name }
+
+            assertEquals(expected, VehicleRepository().getAll())
+        }
     }
 }
